e46bf08c1adb18a6071176cd3fc815849863fecd
박민정 <64456391+mjungpp@users.noreply.github.com>
2021-07-07 17:43:17 +0900

    HTML, CSS : 레이아웃 구현중

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const startBtn = document.querySelector('.game__startBtn');
+const backgroundTransparent =
+startBtn.addEventListener('click', () => {
+    if(started){
+        stopGame();
+    }else {
+        startGame();
+    }
+});
+
+function startGame() {
+    started = true;
+    hideStartContent();
+}
+
+function hideStartContent() {
+
+}
+
+function stopGame() {
+    started = false;
+}
