1d523d41a26f2d30dfef758da11929e851a4e66a
박민정 <64456391+mjungpp@users.noreply.github.com>
2021-07-08 17:59:11 +0900

    HTML, CSS : 레이아웃 구현중

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const startBtn = document.querySelector('.game__startBtn');
-const backgroundTransparent =
+const startContent = document.querySelector('.game__contents');
+let started = false;
+const gameTimer = document.querySelector('.game__timer');
+
 startBtn.addEventListener('click', () => {
     if(started){
         stopGame();
@@ -12,10 +15,25 @@ startBtn.addEventListener('click', () => {
 
 function startGame() {
     started = true;
+    console.log('start');
     hideStartContent();
+    showGameTimer();
+    playSound();
 }
 
 function hideStartContent() {
+    startContent.remove();
+}
+
+function showGameTimer() {
+    gameTimer.style.display = "block";
+}
+
+function stopGameTimer() {
+
+}
+
+function playSound() {
 
 }
 
