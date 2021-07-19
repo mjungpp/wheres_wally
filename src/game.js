a17da8a235670d158a7464297ca28ee4d3bf6e0e
박민정 <64456391+mjungpp@users.noreply.github.com>
2021-07-19 14:19:31 +0900

    JS : Game.js 모듈화 진행중

---
+++ src/game.js
@@ -0,0 +1,108 @@
+'use strict';
+import * as sound from './sound.js';
+import Field from './field.js';
+
+export default class Game {
+    constructor(gameDuration){
+        this.gameDuration = gameDuration;
+        this.gameTimer = document.querySelector('.game__timer');
+        this.started = false;
+        this.timer = undefined;
+        this.level = 0;
+        this.gameField = new Field();
+        this.gameField.setClickListener(this.onFieldClick);
+        this.startBtn = document.querySelector('.game__startBtn');
+        this.startBtn.addEventListener('click', this.start);
+    }
+
+    onFieldClick = (stage, win) => {
+        if(!this.started){
+            return;
+        }
+        if(this.level == 0){
+            if(stage == 'first' && win == 'win'){
+                this.finish('win');
+            }else if(stage == 'first' && win == 'lose'){
+                this.finish('lose');
+            }
+        }
+        if(this.level == 1){
+            if(stage == 'second' && win == 'win'){
+                this.finish('win');
+            }else if(stage == 'second' && win == 'lose'){
+                this.finish('lose');
+            }
+        }
+        if(this.level == 2){
+            if(stage == 'third' && win == 'win'){
+                this.finish('finish');
+            }else if(stage == 'third' && win == 'lose'){
+                this.finish('lose');
+            }
+        }
+    }
+
+    setGameStopListener(onGameStop){
+        this.onGameStop = onGameStop;
+    }
+    
+    start() {
+        this.started = true;
+        this.initTimer();
+        this.gameField.hideStartContent;
+        this.setBackgroundImg();
+        this.showTimer();
+        sound.playBackground();
+        this.startTimer();
+    }
+    
+    finish(win){
+        this.started = false;
+        this.stopTimer();
+        sound.stopBackground();
+        if(win == 'win' || win == 'finish'){
+            sound.playWin();
+        }
+        if(win == 'lose') {
+            sound.playAlert();
+        }
+        this.onGameStop && this.onGameStop(win);
+    }
+    
+    initTimer(){
+        this.timer = undefined;
+    }
+
+    setBackgroundImg(){
+        let imgPath = `url(img/game__field/stage${this.level}.jpg)`;
+        this.gameField.setBackgroundImage(imgPath);
+    }
+
+    showTimer() {
+        this.gameTimer.style.visibility = 'visible';
+    }
+
+    startTimer() {
+        let remainingTimeSec = this.gameDuration;
+        this.updateTimerText(remainingTimeSec);
+        this.timer = setInterval(() => {
+            if(remainingTimeSec <= 0) {
+                clearInterval(timer);
+                sound.stopBackground();
+                this.finish('lose');
+                return;
+            }
+            this.updateTimerText(--remainingTimeSec);
+        }, 1000);
+    }
+
+    updateTimerText(time) {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        this.gameTimer.innerHTML = `${minutes}:${seconds}`;
+    }
+
+    stopTimer() {
+        clearInterval(this.timer);
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,53 +1,11 @@
 'use strict';
-
-import * as sound from './sound.js';
 import PopUp from './popup.js';
-import Field from './field.js';
-
-const startBtn = document.querySelector('.game__startBtn');
-const gameTimer = document.querySelector('.game__timer');
-
-// game
-let started = false;
-let timer = undefined;
-const GAME_DURATION_SEC = 150;
-let level = 0;
-
-startBtn.addEventListener('click', startGame);
+import Game from './game.js';
 
 const gameFinishBanner = new PopUp();
 gameFinishBanner.setClickListner((button) => {
     onItemClick(button);
 });
-
-const gameField = new Field();
-gameField.setClickListener(onFieldClick);
-function onFieldClick(stage, win){
-    if(!started){
-        return;
-    }
-    if(level == 0){
-        if(stage == 'first' && win == 'win'){
-            finishGame('win');
-        }else if(stage == 'first' && win == 'lose'){
-            finishGame('lose');
-        }
-    }
-    if(level == 1){
-        if(stage == 'second' && win == 'win'){
-            finishGame('win');
-        }else if(stage == 'second' && win == 'lose'){
-            finishGame('lose');
-        }
-    }
-    if(level == 2){
-        if(stage == 'third' && win == 'win'){
-            finishGame('finish');
-        }else if(stage == 'third' && win == 'lose'){
-            finishGame('lose');
-        }
-    }
-}
 
 function onItemClick(button) {
     if(button == 'next'){
@@ -59,67 +17,7 @@ function onItemClick(button) {
     startGame();
 }
 
-function startGame() {
-    started = true;
-    initTimer();
-    gameField.hideStartContent();
-    setBackgroundImg();
-    showGameTimer();
-    sound.playBackground();
-    startGameTimer();
-}
-
-// game.js
-function initTimer() {
-    timer = undefined;
-}
-
-// field.js
-function setBackgroundImg(){
-    let imgPath = `url(img/game__field/stage${level}.jpg)`;
-    gameField.setBackgroundImage(imgPath);
-}
-
-function showGameTimer() {
-    gameTimer.style.visibility = 'visible';
-}
-
-// game.js
-function startGameTimer() {
-    let remainingTimeSec = GAME_DURATION_SEC;
-    updateTimerText(remainingTimeSec);
-    timer = setInterval(() => {
-        if(remainingTimeSec <= 0) {
-            clearInterval(timer);
-            sound.stopBackground();
-            finishGame('lose');
-            return;
-        }
-        updateTimerText(--remainingTimeSec);
-    }, 1000);
-}
-
-function updateTimerText(time) {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    gameTimer.innerHTML = `${minutes}:${seconds}`;
-}
-
-// game.js
-function stopGameTimer() {
-    clearInterval(timer);
-}
-
-// game.js
-function finishGame(win){
-    started = false;
-    stopGameTimer();
-    sound.stopBackground();
-    if(win == 'win' || win == 'finish'){
-        sound.playWin();
-    }
-    if(win == 'lose') {
-        sound.playAlert();
-    }
-    gameFinishBanner.showWithText(win);
-}
+const game = new Game(150);
+game.setGameStopListener((reason) => {
+    console.log(reason);
+});
